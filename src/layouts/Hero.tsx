4df62e6904c0e3ba7762d4325e1ba170f68964a2
soustern <rafaelantoniassivicechio@gmail.com>
2025-09-27 19:28:13 -0300

    Agg grainy texture to background

--- src/layouts/Hero.tsx
+++ src/layouts/Hero.tsx
@@ -23,8 +23,8 @@ const Hero = (): JSX.Element => {
     
     return (
         <section id='hero' className='flex flex-col h-screen items-center justify-center py-12 md:py-16 lg:py-24'>
-            <div ref={containerRef} className="[&>canvas]:absolute [&>canvas]:left-1/2 [&>canvas]:top-1/2 [&>canvas]:transform [&>canvas]:-translate-1/2 [&>canvas]:rounded-lg relative container rounded-lg overflow-hidden w-110 h-110 flex pointer-events-auto mb-8">
-                <video ref={videoRef} autoPlay muted playsInline loop className='object-fill h-110 w-110' src="src\assets\videos\hero.mp4"></video>
+            <div ref={containerRef} className="[&>canvas]:absolute [&>canvas]:left-1/2 [&>canvas]:top-1/2 [&>canvas]:transform [&>canvas]:-translate-1/2 [&>canvas]:rounded-lg relative container rounded-lg overflow-hidden w-70 h-70 sm:w-70 sm:h-70 md:w-90 md:h-90 lg:w-110 lg:h-110 flex pointer-events-auto mb-8">
+                <video ref={videoRef} autoPlay muted playsInline loop className='object-fill' src="src\assets\videos\hero.mp4"></video>
             </div>
             <div className="space-y-6">
                 <TextHeadline className="font-mono"  text={""}><span className="text-[var(--color-accent-primary)]">&lt;</span>Future.Ready.Developer<span className="text-[var(--color-accent-primary)]">/&gt;</span></TextHeadline>
