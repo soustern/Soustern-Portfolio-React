c16e19f0534918f879b3c53323572fdb792b5e45
soustern <rafaelantoniassivicechio@gmail.com>
2025-09-30 14:52:12 -0300

    Create hero subHeading

--- src/layouts/Hero.tsx
+++ src/layouts/Hero.tsx
@@ -6,6 +6,9 @@ import { useLanguage } from "../components/contexts/LanguageContext";
 import TextStandard from "../components/ui/TextStandard";
 import PrimaryIcon from "../components/ui/PrimaryIcons";
 import { AnimatePresence, motion } from "motion/react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 
 const Hero = (): JSX.Element => {
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -13,6 +16,8 @@ const Hero = (): JSX.Element => {
     const { language } = useLanguage();
     const hoverTip = useRef<HTMLDivElement>(null);
     const [isHovered, setIsHovered] = useState(false);
+    const scrollToExploreLeft = useRef<HTMLDivElement>(null);
+    const scrollToExploreRight = useRef<HTMLDivElement>(null);
 
     const strings = {
         heading: () => {
@@ -78,13 +83,28 @@ const Hero = (): JSX.Element => {
         };
     }, [language]); // Re-run when language changes
     
+    useGSAP(() => {
+        gsap.fromTo(scrollToExploreLeft.current, {x: -180, opacity: 1}, {x: 180, opacity: 1, duration: 2.5, ease: "power3.out", delay: 1, repeat: -1, repeatDelay: 4});
+
+        gsap.fromTo(scrollToExploreRight.current, {x: 180, opacity: 1}, {x: -180, opacity: 1, duration: 2.5, ease: "power3.out", delay: 1, repeat: -1, repeatDelay: 4});
+    })
+
     return (
         <section id='hero' className='flex flex-col h-screen items-center relative justify-center py-12 md:py-16 lg:py-24'>
             <div ref={containerRef} className="modern-arch z-10 [&>canvas]:absolute [&>canvas]:left-1/2 [&>canvas]:top-1/2 [&>canvas]:transform [&>canvas]:-translate-1/2 relative container rounded-lg overflow-hidden w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] flex pointer-events-auto mb-8 ">
                 <video ref={videoRef} autoPlay muted playsInline loop className='object-fill' src="src\assets\videos\hero.mp4"></video>
             </div>
-            <div className="space-y-6 z-10 relative">
+            <div className="space-y-4 z-10 relative">
                 <TextHeadline className="font-mono" text={""}><span className="text-[var(--color-accent-primary)]">&lt;</span>{strings.heading()}<span className="text-[var(--color-accent-primary)]">/&gt;</span></TextHeadline>
+                <div className="flex items-center w-full gap-2">
+                    <div className="flex-1 h-[1px] bg-[var(--color-bg-tertiary)] overflow-hidden relative rounded-full">
+                        <div ref={scrollToExploreLeft} className="absolute transform -translate-y-1/2  w-full h-full bg-[radial-gradient(100px_circle,#f9fafb,transparent_40%)]"></div>
+                    </div>
+                    <TextStandard text="Scroll to explore" className="" importance="supporting"></TextStandard>
+                    <div className="flex-1 h-[1px] bg-[var(--color-bg-tertiary)] overflow-hidden relative rounded-full">
+                        <div ref={scrollToExploreRight} className="absolute transform -translate-y-1/2 w-full h-full bg-[radial-gradient(100px_circle,#f9fafb,transparent_40%)]"></div>
+                    </div>
+                </div>
                 <div id="hover-tip" className="absolute left-0 -top-75 w-full opacity-0 lg:opacity-100">   
                     <AnimatePresence>
                         {isHovered && <motion.div initial={{opacity: 0, scale: 0.9}} animate={{opacity: 1, scale: 1}} exit={{ opacity: 0,    scale: 0.9 }} transition={{ duration: 0.2 }} className={`absolute ${language === `En` ? `-left-26` : `-left-30`}  top-0.5 w-fit`}>
