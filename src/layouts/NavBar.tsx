2d81525ac1f584c81efc060f0f9188d7156ccc05
soustern <rafaelantoniassivicechio@gmail.com>
2025-11-04 15:45:03 -0300

    Prototype 1

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -12,7 +12,9 @@ import { FaGithub } from 'react-icons/fa6';
 import { FaLinkedin } from 'react-icons/fa6';
 import { FaXmark } from 'react-icons/fa6';
 import { FaPlus } from 'react-icons/fa6';
-
+import PrimaryButton from "../components/ui/PrimaryButton";
+import { FaBars } from 'react-icons/fa6';
+import { FaArrowRight } from 'react-icons/fa6';
 
 function NavBar(): JSX.Element  {    
     const [isOpen, setIsOpen] = useState(false);
@@ -100,7 +102,9 @@ function NavBar(): JSX.Element  {
             },
         }
     }
-    
+
+    const navOptions = [["Home", "hero-section"], ["A Prance", "about-section"], ["Serviços", "services-section"], ["Clientes", "brands-section"], ["Contatos", "footer-section"]];
+
 
     const languageMenu = (): ReactNode => {
         if (isOpen && ref.current)
@@ -153,7 +157,57 @@ function NavBar(): JSX.Element  {
 
     if (screenWidth.width <= 1200)
     {
-        return (<></>);   
+        return (
+            
+            <nav className="bg-[var(--color-bg-primary)]/60 backdrop-blur-3xl px-2 pointer-events-auto">
+                <div className="flex items-center justify-between">
+                    <div id="logo">
+                        <img src={""} className="max-w-[120px]" alt="" />
+                    </div>
+                    <div id="hamburger">
+                        <motion.button aria-expanded={isOpen} aria-label="Abrir o menu" aria-controls="menu" whileTap={{scale: 0.8}} transition={{duration: 0.05, type: "spring", stiffness: 500, damping: 30}} className="cursor-pointer py-3" onClick={() => setIsOpen(!isOpen)}>
+                        {
+                            isOpen ? <FaXmark className="fa-solid fa-xmark text-slate-200 text-4xl"></FaXmark> : <FaBars className="fa-solid fa-bars text-slate-200 text-4xl"></FaBars>
+                        }
+                        </motion.button>
+                    </div>
+                </div>
+                <AnimatePresence mode="wait">{isOpen && <motion.div id="menu" initial={{height: 0}} animate={{height: "auto"}} transition={{duration: 0.2, ease: "easeOut"}} className=" w-full overflow-hidden will-change-transform" exit={{height: 0}} style={{contain: 'layout style paint'}}>
+                    <div className={`px-8 pt-8 pb-30 h-screen overflow-y-auto  ${isOpen ? `pointer-events-auto` : `pointer-events-none`}`}>
+                        <ul className="w-full flex flex-col items-start gap-8">
+                            {
+                                navOptions.map((option, index) => {
+                                    return (
+                                        <motion.li whileTap={{backgroundColor: "rgba(255, 255, 255, 0.3)"}} key={option[0]} className="py-4 w-full relative" onClick={() => setIsOpen(false)}>
+                                            <motion.a href={`#${option[1]}`} className="flex items-center justify-between [will-change: transform, opacity]" initial={{translateY: 10, opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{duration: 0.2, delay: index * 0.08}}>
+                                                <p className="text-slate-200 text-2xl font-medium">{option[0]}</p>
+                                                <FaArrowRight className="fa-solid fa-arrow-right text-slate-300 text-xl"></FaArrowRight>
+                                            </motion.a>
+                                            <motion.div initial={{width: 0}} animate={{width: "100%"}} transition={{duration: 0.2, delay: index * 0.08}}  className="h-[1px] bg-slate-300 left-0 top-full absolute"></motion.div>
+                                        </motion.li>
+                                    )
+                                })
+                            }
+                            <motion.li className="w-full [will-change: transform, opacity]" initial={{translateY: 10, opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.4}}>
+                                <PrimaryButton ariaLabel="Abrir o WhatsApp da empresa" text="Vamos conversar"></PrimaryButton>
+                            </motion.li>
+                            <li className="flex items-center gap-2 w-full justify-center">
+                                <motion.div className="pointer-events-none w-full" initial={{translateY: 10, opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.5}}>
+                                   
+                                </motion.div>
+                                <motion.div className="pointer-events-none w-full" initial={{translateY: 10, opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.6}}>
+                                    
+                                </motion.div>
+                                
+                                <motion.div className="pointer-events-none w-full" initial={{translateY: 10, opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.7}}>
+                                    
+                                </motion.div>
+                            </li>
+                        </ul>
+                    </div>
+                </motion.div>}</AnimatePresence>
+            </nav>
+        ) 
     }
     else
     {
